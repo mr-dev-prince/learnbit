a75678296fb4861d517fd6ac23c2bc1b28e685ac
mr-dev-prince <pkcofficial24@gmail.com>
2026-07-06 13:21:10 +0530

    chore: add debug log for database CA certificate presence

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -6,6 +6,8 @@ const globalForPrisma = globalThis as {
   prisma?: PrismaClient;
 };
 
+console.log('CA Cert', !!process.env.DATABASE_CA_CERT);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
